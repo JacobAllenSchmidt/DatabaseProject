47d9ff7fb08d69256084cbf0f648304fd0d17a8f
Jacob Schmidt <88864274+JacobAllenSchmidt@users.noreply.github.com>
2026-04-16 01:51:23 -0400

    Create login.js
    login.js is a small file that essentially prevents the whole login page from reloading every time the user submits their email to get a 2FA code to.

---
+++ static/scripts/login.js
@@ -0,0 +1,28 @@
+const emailFormCustomer = document.getElementById("email_form_customer");
+const emailFormContractor = document.getElementById("email_form_contractor");
+
+emailFormCustomer.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(emailFormCustomer);
+    
+    const response = await fetch('/login', {
+        method: 'POST',
+        body: data
+    });
+    const result = await response.json();
+    console.log(response);
+    console.log(result);
+});
+
+emailFormContractor.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(emailFormContractor);
+    
+    const response = await fetch('/login', {
+        method: 'POST',
+        body: data
+    });
+    const result = await response.json();
+    console.log(response);
+    console.log(result);
+});
